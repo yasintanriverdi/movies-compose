d947c49d11fb83e32440dc307a6b90bf3ff88c82
Patrick Willmann <patrick.willman@zenjob.com>
2020-12-04 13:47:32 +0100

    Updated Compose to alpha08, agp to 7.0 and refactored the handling of mappers

--- app/src/main/kotlin/com/yasintanriverdi/moviescompose/data/repository/MovieRepository.kt
+++ app/src/main/kotlin/com/yasintanriverdi/moviescompose/data/repository/MovieRepository.kt
@@ -1,24 +1,29 @@
 package com.yasintanriverdi.moviescompose.data.repository
 
 import com.yasintanriverdi.moviescompose.data.datasource.MovieRemoteDataSource
-import com.yasintanriverdi.moviescompose.data.mapper.MovieResponseItemToMovieMapper
-import com.yasintanriverdi.moviescompose.data.mapper.MoviesResponseToMovieListMapper
 import com.yasintanriverdi.moviescompose.model.Movie
 import com.yasintanriverdi.moviescompose.model.RepositoryResult
 import com.yasintanriverdi.moviescompose.model.toRepositoryResult
+import com.yasintanriverdi.moviescompose.network.model.entity.MovieResponseItem
+import com.yasintanriverdi.moviescompose.network.model.entity.toMovie
+import com.yasintanriverdi.moviescompose.network.model.response.MovieListResponse
+import com.yasintanriverdi.moviescompose.network.model.response.toMovieList
 import javax.inject.Inject
 
 class MovieRepository @Inject constructor(
     private val movieRemoteDataSource: MovieRemoteDataSource,
-    private val movieListMapper: MoviesResponseToMovieListMapper,
-    private val movieItemMapper: MovieResponseItemToMovieMapper
 ) {
 
-    suspend fun fetchMovies(page: Int, category: String = CATEGORY_POPULAR): RepositoryResult<List<Movie>> =
-        movieRemoteDataSource.fetchMovies(page, category).toRepositoryResult(movieListMapper)
+    suspend fun fetchMovies(
+        page: Int,
+        category: String = CATEGORY_POPULAR
+    ): RepositoryResult<List<Movie>> =
+        movieRemoteDataSource.fetchMovies(page, category)
+            .toRepositoryResult(MovieListResponse::toMovieList)
 
     suspend fun fetchMovieById(movieId: String): RepositoryResult<Movie> =
-        movieRemoteDataSource.fetchMovieById(movieId).toRepositoryResult(movieItemMapper)
+        movieRemoteDataSource.fetchMovieById(movieId)
+            .toRepositoryResult(MovieResponseItem::toMovie)
 
     companion object {
         const val CATEGORY_POPULAR = "popular"
