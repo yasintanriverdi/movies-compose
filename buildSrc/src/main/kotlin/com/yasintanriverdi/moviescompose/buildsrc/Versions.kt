b562e5ae532dde1191b5174c6ea2d34cdfd7c210
yasintanriverdi <yasin.tanriverdi@zenjob.com>
2020-12-21 22:22:19 +0100

    Version updates

--- buildSrc/src/main/kotlin/com/yasintanriverdi/moviescompose/buildsrc/Versions.kt
+++ buildSrc/src/main/kotlin/com/yasintanriverdi/moviescompose/buildsrc/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     // Kotlin/ Gradle / AGP
-    const val kotlin = "1.4.20"
-    const val androidGradlePlugin = "7.0.0-alpha01"
+    const val kotlin = "1.4.21"
+    const val androidGradlePlugin = "7.0.0-alpha03"
     const val coroutines = "1.4.2"
 
     // AndroidX
@@ -10,25 +10,24 @@ object Versions {
     const val coreKtx = "1.3.2"
     const val lifecycle = "2.2.0"
     const val activity = "1.1.0"
-    const val paging = "3.0.0-alpha09"
+    const val paging = "3.0.0-alpha11"
 
     // Compose
-    const val compose = "1.0.0-alpha08"
-    const val composeNavigation = "1.0.0-alpha03"
-    const val composePaging = "1.0.0-alpha03"
+    const val compose = "1.0.0-alpha09"
+    const val composeNavigation = "1.0.0-alpha04"
+    const val composePaging = "1.0.0-alpha04"
     // Compose Helper
-    const val accompanist = "0.4.0"
+    const val accompanist = "0.4.1"
 
     // Google
-    const val material = "1.1.0"
-    const val hilt = "2.29-alpha"
-    const val hiltLifecycle = "1.0.0-alpha01"
+    const val material = "1.2.1"
+    const val hilt = "2.30.1-alpha"
+    const val hiltLifecycle = "1.0.0-alpha02"
     const val retrofit = "2.9.0"
     const val okHttp = "4.9.0"
 
 
-
     // Plugins
-    const val spotless = "5.7.0"
+    const val spotless = "5.8.2"
 
 }
